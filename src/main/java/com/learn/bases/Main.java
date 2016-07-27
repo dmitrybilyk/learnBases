173277cde1e26ff59f797402d4e93847fdea7f19
Bilyk <dmitry.b@scopicsoftware.com>
2016-07-27 14:59:40 +0300

    Commited immproved version

--- src/main/java/com/learn/bases/Main.java
+++ src/main/java/com/learn/bases/Main.java
@@ -6,36 +6,35 @@ package com.learn.bases;
 public class Main {
   public static void main(String[] args) {
     int iterCount = 0;
-//    for (int i = 2; i <= 100; i++) {
-//      System.out.print("Dividers (Делители) " + i + ": ");
-//
-//      if (i % 2 != 0) {
-//        for (int j = 1; j < i; j+=2) {
-//          iterCount ++;
-//          if ((i % j) == 0) {
-//            System.out.print(j + " ");
-//          }
-//        }
-//      } else {
-//        for (int j = 2; j < i; j+=2) {
-//          iterCount ++;
-//          if ((i % j) == 0) {
-//            System.out.print(j + " ");
-//          }
-//        }
-//      }
-//
-//      System.out.println();
-//
-//    }
     for (int i = 2; i <= 100; i++) {
       System.out.print("Dividers (Делители) " + i + ": ");
-      for (int j = 2; j < i; j++) {
-        iterCount ++;
-        if ((i % j) == 0) System.out.print(j + " ");
+
+      int initialValue;
+      if (isEven(i)) {
+        initialValue = 2;
+      } else {
+        initialValue = 1;
+      }
+      for (int j = initialValue; j < i; j += 2) {
+        iterCount++;
+        if ((i % j) == 0) {
+          System.out.print(j + " ");
+        }
       }
       System.out.println();
     }
+//    for (int i = 2; i <= 100; i++) {
+//      System.out.print("Dividers (Делители) " + i + ": ");
+//      for (int j = 2; j < i; j++) {
+//        iterCount ++;
+//        if ((i % j) == 0) System.out.print(j + " ");
+//      }
+//      System.out.println();
+//    }
     System.out.println(iterCount);
+  }
+
+  private static boolean isEven(int i) {
+    return i % 2 != 0;
   }
 }
